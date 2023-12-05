9063e653efbfef7f2012cbacb33de397bae48a76
Gino <gino.gjx@gmail.com>
2023-12-05 18:12:07 +0800

    build: code format

---
+++ build/flutter_easy_permission/generated/source/buildConfig/debug/xyz/bczl/flutter/easy_permission/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package xyz.bczl.flutter.easy_permission;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "xyz.bczl.flutter.easy_permission";
+  public static final String BUILD_TYPE = "debug";
+}
